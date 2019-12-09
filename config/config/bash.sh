2c6a27212d8ee4d562f0f1c1d027324c2778e93a
cafrotos <hotanhiu.99@gmail.com>
2019-12-09 15:18:19 +0700

    add

--- config/config/bash.sh
+++ config/config/bash.sh
@@ -3,12 +3,14 @@ BASE_PATH=$(dirname $0)
 
 echo "Waiting for mysql to get up"
 # Give 60 seconds for master and slave to come up
-sleep 60
+sleep 10
 
 echo "Create MySQL Servers (master / slave repl)"
 echo "-----------------"
-
-
+# 
+# 
+# 
+# Reset all slave on 2 slave server
 echo "* Reset all slave on 2 slave server"
 # slave 1
 mysql --host slave1 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e 'STOP SLAVE;';
@@ -17,6 +19,10 @@ mysql --host slave1 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e 'RESET SLAVE ALL;';
 mysql --host slave2 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e 'STOP SLAVE;';
 mysql --host slave2 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e 'RESET SLAVE ALL;';
 
+# 
+# 
+# 
+#  Create replication user
 echo "* Create replication user"
 # for slave 1
 mysql --host master -uroot \
@@ -31,12 +37,21 @@ mysql --host master -uroot \
 mysql --host master -uroot \
   -p$MYSQL_ROOT_PASSWORD -AN -e 'FLUSH PRIVILEGES;'
 
+# 
+# 
+# 
+# Get MASTER_LOG_POS and MASTER_LOG_FILE from master server
 echo "* Get MASTER_LOG_POS and MASTER_LOG_FILE from master server"
 MASTER_LOG_POS=$(eval "mysql --host master -uroot \
   -p$MYSQL_ROOT_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
 MASTER_LOG_FILE=$(eval "mysql --host master -uroot \
   -p$MYSQL_ROOT_PASSWORD -e 'show master status \G' | grep File | sed -n -e 's/^.*: //p'")
 
+# 
+# 
+# 
+# 
+# Login replica user in slave server
 echo "* Login replica user in slave server"
 # slave 1
 mysql --host slave1 -uroot \
@@ -51,6 +66,10 @@ mysql --host slave2 -uroot \
   master_log_file='$MASTER_LOG_FILE', 
   master_log_pos=$MASTER_LOG_POS;"
 
+# 
+# 
+# 
+# Start both Slave Servers
 echo "* Start both Slave Servers"
 mysql --host slave1 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e "START SLAVE;"
 mysql --host slave2 -uroot -p$MYSQL_ROOT_PASSWORD -AN -e "START SLAVE;"
--- config/config/startup.sql
+++ config/config/startup.sql
@@ -7,10 +7,63 @@ DROP SCHEMA IF EXISTS shopmanagement;
 CREATE SCHEMA shopmanagement;
 USE shopmanagement;
 
-CREATE TABLE users (
+CREATE TABLE customers (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  firstname VARCHAR(30) NOT NULL,
-  lastname VARCHAR(30) NOT NULL,
-  email VARCHAR(50),
-  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50)
+);
+
+CREATE TABLE employees (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
+
+CREATE TABLE payments (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
+
+CREATE TABLE product_category (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
+
+CREATE TABLE products (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
+
+CREATE TABLE order_detials (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
+
+CREATE TABLE orders (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  phone VARCHAR(50),
+  'address' VARCHAR(50),
+  'type' VARCHAR(50)
+);
